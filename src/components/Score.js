041157c57a004480173fec25533ec42ebe1f6d3d
aaronlrv <aaronvitharanage@gmail.com>
2022-08-06 22:28:53 +0400

    Style footer

--- src/components/Score.js
+++ src/components/Score.js
@@ -13,10 +13,15 @@ function Score({ link }) {
   }
 
   return (
-    <div>
-      <div>`Score:{score}`</div>
-      <div></div>
-    </div>
+    <>
+      <div class="bg-[rgba(0,0,0,0.4)] h-full ">
+        <div class=" text-white flex justify-center items-center flex-col opacity-100 sticky">
+          <p class="font-mouse text-7xl">Score:{score}</p>
+          <p class="font-mouse text-2xl">Max score: 18</p>
+          <p class="font-mouse text-xl">© Copyright Aaron Lawrence</p>
+        </div>
+      </div>
+    </>
   );
 }
 export default Score;
