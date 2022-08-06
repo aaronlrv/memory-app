b20a2a6cd3195ffbda7442d03683a909fe46e892
aaronlrv <aaronvitharanage@gmail.com>
2022-08-06 22:07:50 +0400

    Import font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: [
+    "order-1",
+    "order-2",
+    "order-3",
+    "order-4",
+    "order-5",
+    "order-6",
+    "order-7",
+    "order-8",
+    "order-9",
+    "order-10",
+    "order-11",
+    "order-12",
+  ],
 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mouse: ["Mouse Memoirs", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
